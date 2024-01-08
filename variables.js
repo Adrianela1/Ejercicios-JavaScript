b40c071dadb38ee46c52251c60e8cae334072f45
Adrianela1 <nemesiososo@gmail.com>
2024-01-08 16:23:09 -0600

    ejercicios terminados

---
+++ variables.js
@@ -0,0 +1,66 @@
+// Variables numéricas
+var numeroVar = 42;
+let numeroLet = 3.14;
+const numeroConst = -7;
+
+// Variables de cadena (string)
+var cadenaVar = "Hola, mundo!";
+let cadenaLet = 'JavaScript';
+const cadenaConst = `Variable constante`;
+
+// Variables booleanas
+var booleanoVar = true;
+let booleanoLet = false;
+const booleanoConst = true;
+
+// Variables de tipo undefined
+var indefinidoVar;
+let indefinidoLet;
+const indefinidoConst = undefined;
+
+
+// Declarar variables numéricas
+var numero1 = 10;
+var numero2 = 5;
+
+// Realizar operaciones aritméticas básicas
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var division = numero1 / numero2;
+
+// Mostrar los resultados en la consola
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);  
+console.log("División: " + division);     
+
+// Declarar variables con cadenas de texto
+var mensaje1 = "Hola, ";
+var mensaje2 = "bienvenido ";
+var mensaje3 = "a ";
+var mensaje4 = "JavaScript.";
+var mensaje5 = "¡Es emocionante!";
+
+// Concatenar las variables y mostrar en pantalla
+console.log(mensaje1 + mensaje2 + mensaje3 + mensaje4 + mensaje5);
+// Declarar 5 variables de diferentes tipos
+var numero = 42;
+var cadena = "25";
+var booleano = true;
+var array = [1, 2, 3];
+var objeto = { nombre: "John", edad: 30 };
+
+// Convertir variables a otro tipo de dato
+var numeroConvertido = String(numero);
+var cadenaConvertida = Number(cadena);
+var booleanoConvertido = String(booleano);
+var arrayConvertido = String(array);
+var objetoConvertido = JSON.stringify(objeto);
+
+// Mostrar los resultados en la consola
+console.log("Número convertido a cadena:", numeroConvertido);
+console.log("Cadena convertida a número:", cadenaConvertida);
+console.log("Booleano convertido a cadena:", booleanoConvertido);
+console.log("Array convertido a cadena:", arrayConvertido);
+console.log("Objeto convertido a cadena (JSON):", objetoConvertido);
