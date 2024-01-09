67af2ea094bb012561b65e4df5785b27c98676af
Adrianela1 <nemesiososo@gmail.com>
2024-01-09 13:03:28 -0600

    ejercicios funciones

---
+++ funciones.js
@@ -0,0 +1,25 @@
+// Calcular el perímetro de un círculo
+function calcularPerimetroCirculo(radio) {
+    return 2 * Math.PI * radio;
+  }
+  
+  // Calcular el área de un rectángulo
+  function calcularAreaRectangulo(ancho, largo) {
+    return ancho * largo;
+  }
+  
+  // Calcular el cuadrado de un número
+  const calcularCuadrado = (numero) => numero ** 2;
+  
+  // Calcular la conversión de grados Celsius a Fahrenheit
+  function celsiusToFahrenheit(gradosCelsius) {
+    return (gradosCelsius * 9/5) + 32;
+  }
+  
+  // Calcular el valor del voltaje dadas la resistencia y la corriente
+  const calcularVoltaje = (resistencia, corriente) => resistencia * corriente;
+  
+  // Calcular el volumen de una esfera
+  function calcularVolumenEsfera(radio) {
+    return (4/3) * Math.PI * Math.pow(radio, 3);
+  }
